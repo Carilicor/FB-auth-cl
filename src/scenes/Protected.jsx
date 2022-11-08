69fda5d96ee38b2a576c9838cb59b085280a6ae5
Cari Licor <Carilicor@gmail.com>
2022-11-08 11:06:52 -0500

    project-FB-Auth

---
+++ src/scenes/Protected.jsx
@@ -0,0 +1,8 @@
+export default function Protected() {
+    return (
+        <>
+            <h1>Protected</h1>
+            <p>Wecome User. This is the hottest secret site around</p>
+        </>
+    )
+}
